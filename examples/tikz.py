68f4848fbb51b234152575416ab7674d4e37e8f5
Rafael Ballester <rballester@ifi.uzh.ch>
2017-12-05 17:22:01 +0100

    Tikz import

--- examples/tikz.py
+++ examples/tikz.py
@@ -10,12 +10,13 @@ from __future__ import (absolute_import, division,
 import numpy as np
 import tt
 import ttrecipes as tr
+import ttrecipes.tikz
 np.random.seed(1)
 
 
 X = np.random.randn(3, 4, 3, 4)
 t = tt.vector(X, eps=0.0)
-output_file = 'tikz_example.tex')
+output_file = 'tikz_example.tex'
 selected = [1, 3, 0, 1]
 
 tr.tikz.draw(t=t, output_file=output_file, rank_labels=True, core_sep=5.5, colormap='autumn', figure_decorator=False, letter='T', selected=selected)
