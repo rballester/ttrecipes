5575e03c40ac302a303f68d184b91e60f0f6b091
Rafael Ballester <rballester@ifi.uzh.ch>
2017-12-19 11:46:58 +0100

    Fi

--- ttrecipes/core/__init__.py
+++ ttrecipes/core/__init__.py
@@ -4,7 +4,6 @@ from .analysis import *
 from .completion import *
 from .compression import *
 from .cross import *
-from .games import *
 from .masks import *
 from .stats import *
 from .sensitivity import *
